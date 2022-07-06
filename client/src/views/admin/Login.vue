d8668d01a7f14afe94e68c5f222e9b0777f9a9a5
ThamerAyachi <thamerjak3@gmail.com>
2022-07-06 01:21:47 +0100

    adding cards in dashboard page

--- client/src/views/admin/Login.vue
+++ client/src/views/admin/Login.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center justify-center h-screen px-6 bg-green-50">
     <div class="w-full max-w-sm p-6 bg-white rounded-md shadow-md">
       <div class="flex items-center justify-center space-x-1">
-        <img src="img/logo.png" alt="logo" class="w-10">
+        <img src="img/logo.png" alt="logo" class="w-10" />
         <span class="text-2xl font-semibold text-gray-700">Admin Fashion</span>
       </div>
 
@@ -27,17 +27,20 @@
           />
         </label>
 
-        <div class="bg-red-100 rounded-lg py-3 mt-2 px-6 text-base text-red-700 mb-3" v-if="v$.$error">
+        <div
+          class="bg-red-100 rounded-lg py-3 mt-2 px-6 text-base text-red-700 mb-3"
+          v-if="v$.$error"
+        >
           Values is not a valid
         </div>
-
 
         <div class="mt-6">
           <button
             type="submit"
-            class="w-full px-4 py-2 text-sm text-center text-white bg-primary rounded-md focus:outline-none hover:bg-green-400"
+            class="w-full px-4 py-2 text-sm text-center text-white bg-primary rounded-md focus:outline-none hover:bg-green-400 space-x-1"
           >
-            Sign in
+            <fa-icon icon="right-to-bracket" />
+            <span>Sign in</span>
           </button>
         </div>
       </form>
@@ -46,32 +49,32 @@
 </template>
 
 <script>
-import useValidate from '@vuelidate/core'
-import { required, email } from '@vuelidate/validators'
+import useValidate from "@vuelidate/core";
+import { required, email } from "@vuelidate/validators";
 
 export default {
-    data(){
-        return{
-          v$: useValidate(),
-            email: '',
-            password: '',
-        }
-    },
-    methods:{
-        signUp(){
-            this.v$.$validate()
-            if (!this.v$.$error) {
-                console.log('submit')
-            }else{
-              console.log(this.v$.$errors)
-            }
-        }
-    },
-    validations() {
-      return{
-        email: { required, email },
-        password: { required }
+  data() {
+    return {
+      v$: useValidate(),
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    signUp() {
+      this.v$.$validate();
+      if (!this.v$.$error) {
+        console.log("submit");
+      } else {
+        console.log(this.v$.$errors);
       }
-    }
-}
+    },
+  },
+  validations() {
+    return {
+      email: { required, email },
+      password: { required },
+    };
+  },
+};
 </script>
--- client/src/views/test.vue
+++ client/src/views/test.vue
@@ -1,29 +1,30 @@
 <template>
   <div class="w-full h-screen flex justify-center items-center space-x-5">
-      <fa-icon icon="tag" class="text-gray-600 fa-2x" :spin="false"/>
-      <fa-layer class="fa-4x">
-          <fa-icon icon="circle" class="text-red-600"></fa-icon>
-          <fa-icon icon="times" transform="shrink-8" inverse></fa-icon>
-      </fa-layer>
+    <fa-icon icon="tag" class="text-gray-600 fa-2x" :spin="false" />
+    <fa-layer class="fa-4x">
+      <fa-icon icon="circle" class="text-red-600"></fa-icon>
+      <fa-icon icon="times" transform="shrink-8" inverse></fa-icon>
+    </fa-layer>
 
-      <fa-layer class="text-gray-600 fa-3x">
-          <fa-icon icon="tag"></fa-icon>
-          <fa-layer-text value="NEW" transform="shrink-13 rotate-45" class="text-white"></fa-layer-text>
-      </fa-layer>
+    <fa-layer class="text-gray-600 fa-3x">
+      <fa-icon icon="tag"></fa-icon>
+      <fa-layer-text
+        value="NEW"
+        transform="shrink-13 rotate-45"
+        class="text-white"
+      ></fa-layer-text>
+    </fa-layer>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            isSpin: true
-        }
-    }
-
-}
+  data() {
+    return {
+      isSpin: true,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
