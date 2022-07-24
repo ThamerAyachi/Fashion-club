f75e9e7700743fd9010bd72311b8fe8183f1a9e7
Thamer Ayachi <thamerjak3@gmail.com>
2022-07-24 19:04:58 +0100

    add table users in users page

--- client/src/views/admin/Users.vue
+++ client/src/views/admin/Users.vue
@@ -1,9 +1,216 @@
 <template>
-  <div>Users</div>
+  <div>
+    <div>
+      <div class="mt-8">
+        <div class="mt-6">
+          <h2 class="text-xl font-semibold leading-tight text-gray-700">
+            Users
+          </h2>
+
+          <div class="flex flex-col mt-3 sm:flex-row">
+            <div class="flex">
+              <div class="relative">
+                <select
+                  class="block w-full h-full px-4 py-2 pr-8 leading-tight text-gray-700 bg-white border border-gray-400 rounded-l appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+                >
+                  <option>5</option>
+                  <option>10</option>
+                  <option>20</option>
+                </select>
+              </div>
+
+              <div class="relative">
+                <select
+                  class="block w-full h-full px-4 py-2 pr-8 leading-tight text-gray-700 bg-white border-t border-b border-r border-gray-400 rounded-r appearance-none sm:rounded-r-none sm:border-r-0 focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500"
+                >
+                  <option>All</option>
+                  <option>Active</option>
+                  <option>Inactive</option>
+                </select>
+              </div>
+            </div>
+
+            <div class="relative block mt-2 sm:mt-0">
+              <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                <svg
+                  viewBox="0 0 24 24"
+                  class="w-4 h-4 text-gray-500 fill-current"
+                >
+                  <path
+                    d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"
+                  />
+                </svg>
+              </span>
+
+              <input
+                placeholder="Search"
+                class="block w-full py-2 pl-8 pr-6 text-sm text-gray-700 placeholder-gray-400 bg-white border border-b border-gray-400 rounded-l rounded-r appearance-none sm:rounded-l-none focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
+              />
+            </div>
+          </div>
+
+          <div class="px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8">
+            <div
+              class="inline-block min-w-full overflow-hidden rounded-lg shadow"
+            >
+              <table class="min-w-full leading-normal">
+                <thead>
+                  <tr>
+                    <th
+                      class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200"
+                    >
+                      User
+                    </th>
+                    <th
+                      class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200"
+                    >
+                      Role
+                    </th>
+                    <th
+                      class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200"
+                    >
+                      Created at
+                    </th>
+                    <th
+                      class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200"
+                    >
+                      Updated at
+                    </th>
+                    <th
+                      class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200"
+                    ></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(u, index) in users" :key="index">
+                    <td
+                      class="px-5 py-5 text-sm bg-white border-b border-gray-200"
+                    >
+                      <div class="flex items-center">
+                        <div class="flex-shrink-0 w-10 h-10">
+                          <img
+                            class="w-full h-full rounded-full"
+                            :src="u.picture"
+                            alt="profile pic"
+                          />
+                        </div>
+
+                        <div class="ml-4">
+                          <div
+                            class="text-sm font-medium leading-5 text-gray-900"
+                          >
+                            {{ u.name }}
+                          </div>
+                          <div class="text-sm leading-5 text-gray-500">
+                            john@example.com
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td
+                      class="px-5 py-5 text-sm bg-white border-b border-gray-200"
+                    >
+                      <p class="text-gray-900 whitespace-nowrap">
+                        {{ u.role }}
+                      </p>
+                    </td>
+                    <td
+                      class="px-5 py-5 text-sm bg-white border-b border-gray-200"
+                    >
+                      <p class="text-gray-900 whitespace-nowrap">
+                        {{ u.created }}
+                      </p>
+                    </td>
+                    <td
+                      class="px-5 py-5 text-sm bg-white border-b border-gray-200"
+                    >
+                      <p class="text-gray-900 whitespace-nowrap">
+                        {{ u.created }}
+                      </p>
+                    </td>
+                    <td
+                      class="px-5 py-5 text-sm bg-white border-b border-gray-200"
+                    >
+                      <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                        >Edit</a
+                      >
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <div
+                class="flex flex-col items-center px-5 py-5 bg-white border-t xs:flex-row xs:justify-between"
+              >
+                <span class="text-xs text-gray-900 xs:text-sm"
+                  >Showing 1 to 4 of 50 Entries</span
+                >
+
+                <div class="inline-flex mt-2 xs:mt-0">
+                  <button
+                    class="px-4 py-2 text-sm font-semibold text-gray-800 bg-gray-300 rounded-l hover:bg-gray-400"
+                  >
+                    Prev
+                  </button>
+                  <button
+                    class="px-4 py-2 text-sm font-semibold text-gray-800 bg-gray-300 rounded-r hover:bg-gray-400"
+                  >
+                    Next
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      users: [
+        {
+          picture:
+            "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80",
+          name: "Vera Carpenter",
+          role: "Admin",
+          created: "Jan 21, 2020",
+          status: "Active",
+          statusColor: "green",
+        },
+        {
+          picture:
+            "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80",
+          name: "Blake Bowman",
+          role: "Editor",
+          created: "Jan 01, 2020",
+          status: "Active",
+          statusColor: "green",
+        },
+        {
+          picture:
+            "https://images.unsplash.com/photo-1540845511934-7721dd7adec3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80",
+          name: "Dana Moore",
+          role: "Editor",
+          created: "Jan 10, 2020",
+          status: "Suspended",
+          statusColor: "orange",
+        },
+        {
+          picture:
+            "https://images.unsplash.com/photo-1522609925277-66fea332c575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&h=160&w=160&q=80",
+          name: "Alonzo Cox",
+          role: "Admin",
+          created: "Jan 18, 2020",
+          status: "Inactive",
+          statusColor: "red",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
