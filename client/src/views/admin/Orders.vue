385badd2e36a56efa1d122a1a9ee4c7cb80c9e35
Thamer Ayachi <thamerjak3@gmail.com>
2022-08-30 22:25:26 +0100

    add model in orders page

--- client/src/views/admin/Orders.vue
+++ client/src/views/admin/Orders.vue
@@ -136,6 +136,104 @@
         </div>
       </div>
     </div>
+
+    <!-- model -->
+    <div>
+      <div
+        :class="`modal ${
+          !open && 'opacity-0 pointer-events-none'
+        } z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center`"
+      >
+        <div
+          @click="open = false"
+          class="absolute w-full h-full bg-gray-900 opacity-50 modal-overlay"
+        ></div>
+
+        <div
+          class="z-50 w-11/12 mx-auto overflow-y-auto bg-white rounded shadow-lg modal-container md:max-w-3xl"
+        >
+          <div
+            class="absolute top-0 right-0 z-50 flex flex-col items-center mt-4 mr-4 text-sm text-white cursor-pointer modal-close"
+          >
+            <svg
+              class="text-white fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 18 18"
+            >
+              <path
+                d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+              />
+            </svg>
+            <span class="text-sm">(Esc)</span>
+          </div>
+
+          <!-- Add margin if you want to see some of the overlay behind the modal-->
+          <div class="px-6 py-4 text-left modal-content">
+            <!--Title-->
+            <div class="flex items-center justify-between pb-3">
+              <p class="text-2xl font-medium text-gray-900">
+                {{ order.fullName }}
+              </p>
+              <div
+                class="z-50 cursor-pointer modal-close"
+                @click="open = false"
+              >
+                <svg
+                  class="text-black fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <!--Body-->
+            <div class="mb-8 mt-6">
+              <div class="flex justify-between mb-8 text-gray-600 text-sm">
+                <div>
+                  <p class="text-gray-900 font-bold text-base">
+                    {{ order.state }}
+                  </p>
+                  <p>{{ order.telephone }}</p>
+                </div>
+                <div>{{ order.createAt }}</div>
+              </div>
+              <!-- message -->
+              <div class="text-gray-800">products</div>
+            </div>
+
+            <!--Footer-->
+            <div class="flex justify-end pt-2">
+              <button
+                @click="open = false"
+                class="p-3 px-6 py-3 mr-2 text-indigo-500 bg-transparent rounded-lg hover:bg-gray-100 hover:text-indigo-400 focus:outline-none"
+              >
+                Close
+              </button>
+              <button
+                class="p-3 px-6 py-3 mr-2 text-red-500 bg-transparent rounded-lg hover:bg-gray-100 hover:text-red-400 focus:outline-none"
+                :disabled="isSending"
+              >
+                <fa-icon
+                  icon="rotate"
+                  class="text-xl"
+                  :spin="true"
+                  v-if="isSending"
+                />
+                <span v-else>Delete</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -152,6 +250,8 @@ export default {
       page: 0,
       open: false,
       select: "all",
+      order: {},
+      isSending: false,
     };
   },
   methods: {
@@ -173,6 +273,10 @@ export default {
     async setData() {
       this.Orders = await this.getOrders();
       this.arrayOrders = showFive(this.Orders);
+    },
+    showOrder(u) {
+      this.order = u;
+      this.open = true;
     },
   },
   async mounted() {
