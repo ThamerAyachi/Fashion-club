042e1154e93825f88c132fabcb70b1146081f5db
Thamer Ayachi <thamerjak3@gmail.com>
2022-09-22 11:54:43 +0100

    add base edit modal

--- client/src/views/admin/Users.vue
+++ client/src/views/admin/Users.vue
@@ -282,6 +282,7 @@
                       <button
                         class="text-indigo-600 hover:text-indigo-900"
                         v-if="role == 'SUPER_ADMIN'"
+                        @click="openEditUser(u)"
                       >
                         Edit
                       </button>
@@ -318,6 +319,76 @@
         </div>
       </div>
     </div>
+
+    <!-- modal -->
+    <div
+      :class="`modal ${
+        !editOpen && 'opacity-0 pointer-events-none'
+      } z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center`"
+    >
+      <div
+        @click="editOpen = false"
+        class="absolute w-full h-full bg-gray-900 opacity-50 modal-overlay"
+      ></div>
+
+      <div
+        class="z-50 w-11/12 mx-auto overflow-y-auto bg-white rounded shadow-lg modal-container md:max-w-md"
+      >
+        <div
+          class="absolute top-0 right-0 z-50 flex flex-col items-center mt-4 mr-4 text-sm text-white cursor-pointer modal-close"
+        >
+          <svg
+            class="text-white fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+          >
+            <path
+              d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+            />
+          </svg>
+          <span class="text-sm">(Esc)</span>
+        </div>
+
+        <!-- Add margin if you want to see some of the overlay behind the modal-->
+        <div class="px-6 py-4 text-left modal-content">
+          <!--Title-->
+          <div class="absolute">
+            <div class="flex items-center justify-end pb-3">
+              <div
+                class="z-50 cursor-pointer modal-close"
+                @click="editOpen = false"
+              >
+                <svg
+                  class="text-black fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <!--Body-->
+          <div>
+            <div class="flex justify-center">
+              <!-- image -->
+              <div
+                class="rounded-full overflow-hidden w-32 h-32 shadow-md bg-gray-100 border-4 border-gray-200"
+              >
+                <img :src="user.imgUrl" class="w-32 rounded-full" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -329,6 +400,7 @@ export default {
   data() {
     return {
       Users: [],
+      user: {},
       role: this.$store.state.role,
       DBUser: [],
       arrayUsers: [],
@@ -348,6 +420,7 @@ export default {
       },
       isSending: false,
       isSuccess: false,
+      editOpen: false,
     };
   },
   methods: {
@@ -436,6 +509,11 @@ export default {
         return "";
       }
       return "hidden";
+    },
+    openEditUser(user) {
+      this.editOpen = true;
+      this.user = user;
+      console.log(user);
     },
   },
   async created() {
